dea92972c4ccf2a85b95be3ff4a953780d725bfa
kietansan <kiemonaddress@gmail.com>
2026-05-07 07:19:37 +0900

    Update cpu.js

--- cpu.js
+++ cpu.js
@@ -1,15 +1,11 @@
 function cpuMove(){
 
-  // 超シンプルCPU（ランダム）
   let x, y;
 
   do {
-    x = Math.floor(Math.random() * SIZE);
-    y = Math.floor(Math.random() * SIZE);
+    x = Math.floor(Math.random()*SIZE);
+    y = Math.floor(Math.random()*SIZE);
   } while(board[y][x] !== 0);
 
-  board[y][x] = 2;
-
-  draw();
-  playSound();
+  return {x, y};
 }
